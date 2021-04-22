1a2971f25c38114c68ec03e9125a38d55d7e61df
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-04-22 09:32:35 +0200

    tried to fix for firebase

---
+++ functions/index.js
@@ -0,0 +1,69 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  let long = req.body.lng;
+  let lat = req.body.lat;
+
+  const url = "https://api.geodatasource.com/city";
+  const apiKey = "SYB4ORNCL6KQTWYWUEE83XDWPVXYQNSZ";
+
+  let requestURL = url.concat("?key=", apiKey, "&lat=", lat, "&lng=", long);
+
+  request(
+    {
+      url: requestURL,
+    },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: "error", message: error });
+      }
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+// app.get('/', (req, res) => {
+//     const lat = 59.31167416666667;
+//     const long = 18.0235053333333;
+//     const url = "https://api.geodatasource.com/city";
+//     const apiKey = "SYB4ORNCL6KQTWYWUEE83XDWPVXYQNSZ";
+
+//     let requestURL = url.concat("?key=",apiKey,"&lat=", lat, "&lng=", long);
+
+//     request(
+//         {
+//           url: requestURL
+//         },
+//         (error, response, body) => {
+//         if (error || response.statusCode !== 200) {
+//           return res.status(500).json({ type: 'error', message: err.message });
+//         }
+//         res.json(JSON.parse(body));
+//       }
+//     )
+//   });
+
+// app.listen(3000, function (err) {
+//   if (!err) {
+//     console.log("app is running on port 3000");
+//   }
+// });
+
+exports.helloWorld = functions.https.onRequest(app);
--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 //Variables for the header
-const header = document.getElementById("header")
+const header = document.getElementById("header");
 const date = document.getElementById("date");
 const time = document.getElementById("time");
 const sunsetPrint = document.getElementById("time-sun");
@@ -44,194 +44,211 @@ let latitude;
 
 function dateToday() {
   const today = new Date();
-  const variations = {weekday : "long", month : "long", day : "numeric", year : "numeric"}
+  const variations = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
   date.innerHTML = today.toLocaleDateString("en-US", variations);
-};
+}
 dateToday();
 
 function timeRightNow() {
-  setInterval(function() {
+  setInterval(function () {
     const todayTime = new Date();
     hours = todayTime.getHours();
     minutes = todayTime.getMinutes();
 
-      if (hours < 10) {
-        hours = '0' + hours
-      }
-      if (minutes < 10) {
-        minutes = '0' + minutes
-      }
-      time.innerHTML = hours + ":" + minutes;
-    }, 1000);
-};
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    time.innerHTML = hours + ":" + minutes;
+  }, 1000);
+}
 timeRightNow();
 
-
 const options = {
-    enableHighAccuracy: false, 
-    maximumAge: 30000, 
-    timeout: 10000
+  enableHighAccuracy: false,
+  maximumAge: 30000,
+  timeout: 10000,
 };
 
-const getLocation = navigator.geolocation.getCurrentPosition(success, error, options);
+const getLocation = navigator.geolocation.getCurrentPosition(
+  success,
+  error,
+  options
+);
 
 function success(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
   getAPIGeoDataSource(latitude, longitude);
-};
+}
 
 function error() {
   displayErrorPageStyles();
   heading.innerText = "Tripped when looking for sunshine :(";
-  counter.innerHTML = "We could not find your location! Please refresh the page and allow us to see your location." + 
-  "<br/>" + "<div id=\"refresh-icon\"><i class=\"fas fa-redo\"></i></div>";
+  counter.innerHTML =
+    "We could not find your location! Please refresh the page and allow us to see your location." +
+    "<br/>" +
+    '<div id="refresh-icon"><i class="fas fa-redo"></i></div>';
 
   const refreshButton = document.getElementById("refresh-icon");
   refreshButton.addEventListener("click", () => {
     window.location.reload();
   });
-};
+}
 
 async function getAPIGeoDataSource(x, y) {
-let lat = x
-let lng = y
+  let lat = x;
+  let lng = y;
 
- await fetch("http://localhost:3000", {
-  method: "post",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  //make sure to serialize your JSON body
-  body: JSON.stringify({
-    lat: lat, 
-    lng: lng
+  await fetch("https://finding-daylight.web.app/", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    //make sure to serialize your JSON body
+    body: JSON.stringify({
+      lat: lat,
+      lng: lng,
+    }),
   })
-})
- .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    sunset = data.sunset;
-    sunrise = data.sunrise;
-    city = data.city;
-    cityPrint.innerText = "Location: " + city;
-    checkTime();
-    heading.classList.remove("hidden");
-    hideLoading.classList.add("hidden");
-    sunsetTimer.classList.remove("hidden");
-    clouds.classList.remove("hidden");
-  })
-  .catch(error => {
-    displayErrorPageStyles();
-    heading.innerText = "Tripped when looking for sunshine :(";
-    counter.innerHTML = "Something went wrong when getting information regarding the sunset or sunrise. Please refresh the page so we can try get it for you again!" + 
-    "<br/>" + "<div id=\"refresh-icon\"><i class=\"fas fa-redo\"></i></div>";
+    .then((response) => response.json())
+    .then((data) => {
+      sunset = data.sunset;
+      sunrise = data.sunrise;
+      city = data.city;
+      cityPrint.innerText = "Location: " + city;
+      checkTime();
+      heading.classList.remove("hidden");
+      hideLoading.classList.add("hidden");
+      sunsetTimer.classList.remove("hidden");
+      clouds.classList.remove("hidden");
+    })
+    .catch((error) => {
+      displayErrorPageStyles();
+      heading.innerText = "Tripped when looking for sunshine :(";
+      counter.innerHTML =
+        "Something went wrong when getting information regarding the sunset or sunrise. Please refresh the page so we can try get it for you again!" +
+        "<br/>" +
+        '<div id="refresh-icon"><i class="fas fa-redo"></i></div>';
 
-    const refreshButton = document.getElementById("refresh-icon");
-    refreshButton.addEventListener("click", () => {
-      window.location.reload();
+      const refreshButton = document.getElementById("refresh-icon");
+      refreshButton.addEventListener("click", () => {
+        window.location.reload();
+      });
     });
-  });
-}; 
+}
 
 function displayErrorPageStyles() {
- document.body.style.background = "var(--dark-gray)";
- sun.style.display = "none";  
- sunsetPrint.style.display = "none";
- clouds.style.display = "none";
+  document.body.style.background = "var(--dark-gray)";
+  sun.style.display = "none";
+  sunsetPrint.style.display = "none";
+  clouds.style.display = "none";
 
- centerText.style.marginTop = "15vh";
- centerText.style.color = "var(--light-peach)";
- date.style.color = "var(--light-peach)";
- time.style.color = "var(--light-peach)";
+  centerText.style.marginTop = "15vh";
+  centerText.style.color = "var(--light-peach)";
+  date.style.color = "var(--light-peach)";
+  time.style.color = "var(--light-peach)";
 
- heading.style.fontSize = "2.5rem";
- heading.style.fontFamily = "var(--font-numbers)";
- counter.style.fontSize = "1.5rem";
+  heading.style.fontSize = "2.5rem";
+  heading.style.fontFamily = "var(--font-numbers)";
+  counter.style.fontSize = "1.5rem";
 
- heading.classList.remove("hidden");
- hideLoading.classList.add("hidden");
- sunsetTimer.classList.remove("hidden");
-};
+  heading.classList.remove("hidden");
+  hideLoading.classList.add("hidden");
+  sunsetTimer.classList.remove("hidden");
+}
 
 function activateNightMode() {
   document.body.style.background = "black";
   header.style.color = "var(--light-peach)";
   sunWarpper.style.top = "0vh";
-  sun.classList.add('moon');
+  sun.classList.add("moon");
   clouds.style.display = "none";
 
   centerText.style.marginTop = "25vh";
   centerText.style.color = "var(--light-peach)";
-  
-  sky[0].classList.add('nightsky');
-  twinkles[0].classList.add('twinkles');
-  
-  mountain1[0].classList.remove('twilight1');
-  mountain4[0].classList.remove('twilight1');
-  mountain7[0].classList.remove('twilight1');
-  mountain10[0].classList.remove('twilight1');
-  mountain2[0].classList.remove('twilight2');
-  mountain5[0].classList.remove('twilight2');
-  mountain8[0].classList.remove('twilight2');
-  mountain3[0].classList.remove('twilight3');
-  mountain6[0].classList.remove('twilight3');
-  mountain9[0].classList.remove('twilight3');
-};
 
+  sky[0].classList.add("nightsky");
+  twinkles[0].classList.add("twinkles");
+
+  mountain1[0].classList.remove("twilight1");
+  mountain4[0].classList.remove("twilight1");
+  mountain7[0].classList.remove("twilight1");
+  mountain10[0].classList.remove("twilight1");
+  mountain2[0].classList.remove("twilight2");
+  mountain5[0].classList.remove("twilight2");
+  mountain8[0].classList.remove("twilight2");
+  mountain3[0].classList.remove("twilight3");
+  mountain6[0].classList.remove("twilight3");
+  mountain9[0].classList.remove("twilight3");
+}
 
 function activateTwilightMode() {
-  document.body.style.background = "linear-gradient(var(--dark-gray), var(--linen))";
+  document.body.style.background =
+    "linear-gradient(var(--dark-gray), var(--linen))";
   sunWarpper.style.top = "35vh";
   centerText.style.color = "var(--light-peach)";
   header.style.color = "var(--light-peach)";
 
-  cloud[0].classList.add('cloud-twilight');
-  cloud[1].classList.add('cloud-twilight');
+  cloud[0].classList.add("cloud-twilight");
+  cloud[1].classList.add("cloud-twilight");
 
-  mountain1[0].classList.add('twilight1');
-  mountain4[0].classList.add('twilight1');
-  mountain7[0].classList.add('twilight1');
-  mountain10[0].classList.add('twilight1');
-  mountain2[0].classList.add('twilight2');
-  mountain5[0].classList.add('twilight2');
-  mountain8[0].classList.add('twilight2');
-  mountain3[0].classList.add('twilight3');
-  mountain6[0].classList.add('twilight3');
-  mountain9[0].classList.add('twilight3');
-};
-
+  mountain1[0].classList.add("twilight1");
+  mountain4[0].classList.add("twilight1");
+  mountain7[0].classList.add("twilight1");
+  mountain10[0].classList.add("twilight1");
+  mountain2[0].classList.add("twilight2");
+  mountain5[0].classList.add("twilight2");
+  mountain8[0].classList.add("twilight2");
+  mountain3[0].classList.add("twilight3");
+  mountain6[0].classList.add("twilight3");
+  mountain9[0].classList.add("twilight3");
+}
 
 function checkTime() {
-  sunsetArr = sunset.split(':');
-  sunriseArr = sunrise.split(':');
+  sunsetArr = sunset.split(":");
+  sunriseArr = sunrise.split(":");
 
-   if (parseInt(hours) > parseInt(sunsetArr[0]) || 
-    parseInt(hours) === parseInt(sunsetArr[0]) && 
-    parseInt(minutes) > parseInt(sunsetArr[1])) {
-      calculateCountDownSunriseBeforeMidnight()
-
-  } else if (parseInt(hours) < parseInt(sunriseArr[0]) || 
-    parseInt(hours) === parseInt(sunriseArr[0]) && 
-    parseInt(minutes) < parseInt(sunsetArr[1])) { 
-      calculateCountDownSunriseAfterMidnight()
-
+  if (
+    parseInt(hours) > parseInt(sunsetArr[0]) ||
+    (parseInt(hours) === parseInt(sunsetArr[0]) &&
+      parseInt(minutes) > parseInt(sunsetArr[1]))
+  ) {
+    calculateCountDownSunriseBeforeMidnight();
+  } else if (
+    parseInt(hours) < parseInt(sunriseArr[0]) ||
+    (parseInt(hours) === parseInt(sunriseArr[0]) &&
+      parseInt(minutes) < parseInt(sunsetArr[1]))
+  ) {
+    calculateCountDownSunriseAfterMidnight();
   } else {
-      calculateCountDownSunset();
-  };
-};
+    calculateCountDownSunset();
+  }
+}
 
-function calculateCountDownSunset () {
+function calculateCountDownSunset() {
   let newDate = new Date();
-  let calculateSunsetDate = newDate.setHours(parseInt(sunsetArr[0]), parseInt(sunsetArr[1]));
+  let calculateSunsetDate = newDate.setHours(
+    parseInt(sunsetArr[0]),
+    parseInt(sunsetArr[1])
+  );
   sunsetPrint.innerText = "Time of Sunset: " + sunset;
 
-  let x = setInterval(function() {
+  let x = setInterval(function () {
     let today = new Date().getTime();
     let distance = calculateSunsetDate - today;
 
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
@@ -243,62 +260,71 @@ function calculateCountDownSunset () {
     } else if (distance <= 0) {
       clearInterval(x);
       calculateCountDownSunriseBeforeMidnight();
-    };
+    }
   }, 1000);
-};
+}
 
 function calculateCountDownSunriseBeforeMidnight() {
   let newDate = new Date();
-  let calculateSunriseDate = newDate.setHours(parseInt(sunriseArr[0]), parseInt(sunriseArr[1]));
+  let calculateSunriseDate = newDate.setHours(
+    parseInt(sunriseArr[0]),
+    parseInt(sunriseArr[1])
+  );
   let calSunTomorrow = new Date(calculateSunriseDate);
   calSunTomorrow.setDate(calSunTomorrow.getDate() + 1);
 
   activateNightMode();
-  sunsetPrint.innerText ="Time of Sunrise: " + sunrise
+  sunsetPrint.innerText = "Time of Sunrise: " + sunrise;
 
-  let y = setInterval(function() {
+  let y = setInterval(function () {
     let today = new Date().getTime();
     let distance = calSunTomorrow - today;
 
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     heading.innerText = "Time until sunrise";
     counter.innerText = hours + "h " + minutes + "m " + seconds + "s ";
 
     if (distance <= 0) {
       clearInterval(y);
-      calculateCountDownSunriseAfterMidnight()
-    };
+      calculateCountDownSunriseAfterMidnight();
+    }
   }, 1000);
-};
+}
 
 function calculateCountDownSunriseAfterMidnight() {
   let newDate = new Date();
-  let calculateSunriseDate = newDate.setHours(parseInt(sunriseArr[0]), parseInt(sunriseArr[1]));
+  let calculateSunriseDate = newDate.setHours(
+    parseInt(sunriseArr[0]),
+    parseInt(sunriseArr[1])
+  );
 
   activateNightMode();
-  sunsetPrint.innerText ="Time of Sunrise: " + sunrise
+  sunsetPrint.innerText = "Time of Sunrise: " + sunrise;
 
-  let y = setInterval(function() {
+  let y = setInterval(function () {
     let today = new Date().getTime();
     let distance = calculateSunriseDate - today;
 
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     heading.innerText = "Time until sunrise";
     counter.innerText = hours + "h " + minutes + "m " + seconds + "s ";
 
     if (distance <= 0) {
       clearInterval(y);
       window.location.reload();
-    };
+    }
   }, 1000);
-};
-
+}
 
 sunWarpper.addEventListener("click", getAPIAdvice);
 
@@ -306,54 +332,49 @@ function getAPIAdvice() {
   const requestAdviceURL = "https://api.adviceslip.com/advice";
 
   fetch(requestAdviceURL)
-  .then(response => response.json())
-  .then(data => {
-    randomAdvice = data.slip.advice;
-    displayAdvice();
-  })
-  .catch(error => {
-    displayAdviceError();
-  });
-};
+    .then((response) => response.json())
+    .then((data) => {
+      randomAdvice = data.slip.advice;
+      displayAdvice();
+    })
+    .catch((error) => {
+      displayAdviceError();
+    });
+}
 
 function displayAdvice() {
-  let printRandomAdvice = document.getElementById("random-advice")
+  let printRandomAdvice = document.getElementById("random-advice");
   let randomAdviceModal = document.getElementById("random-advice-modal");
   randomAdviceModal.style.display = "block";
   printRandomAdvice.innerHTML = randomAdvice;
 
   let close = document.getElementsByClassName("close")[0];
 
-  close.onclick = function() {
+  close.onclick = function () {
     randomAdviceModal.style.display = "none";
   };
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == randomAdviceModal) {
       randomAdviceModal.style.display = "none";
-    };
+    }
   };
-};
+}
 
 function displayAdviceError() {
-  let printRandomAdvice = document.getElementById("random-advice")
+  let printRandomAdvice = document.getElementById("random-advice");
   let randomAdviceModal = document.getElementById("random-advice-modal");
   randomAdviceModal.style.display = "block";
   printRandomAdvice.innerText = "No news are good news.";
 
   let close = document.getElementsByClassName("close")[0];
-  close.onclick = function() {
+  close.onclick = function () {
     randomAdviceModal.style.display = "none";
   };
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == randomAdviceModal) {
       randomAdviceModal.style.display = "none";
-    };
+    }
   };
-};
-
-
-
-
-
+}
